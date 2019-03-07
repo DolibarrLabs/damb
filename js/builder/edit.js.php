5ea9e81845e473a9bb0994d189de89472f6bed67
AXeL-dev <contact.axel.dev@gmail.com>
2019-03-07 01:27:28 +0100

    edit page: replace js confirmation with jquery dialog

--- js/builder/edit.js.php
+++ js/builder/edit.js.php
@@ -14,9 +14,28 @@ header('Content-Type: application/javascript');
 ?>
 
 $(document).ready(function() {
-    $('.delete_module').on('click', function(e) {
-        if (! confirm('<?php echo $langs->trans('ConfirmDeleteModule'); ?>')) {
-            e.preventDefault();
-        }
+    $('a.delete_module').on('click', function(e) {
+        e.preventDefault();
+        var href = $(this).attr('href');
+        $('<div title="<?php echo $langs->trans('DeleteModule'); ?>"><?php echo img_help('', '').' '.$langs->trans('ConfirmDeleteModule'); ?></div>').dialog({
+            autoOpen: true,
+            open: function() {
+                $(this).parent().find("button.ui-button:eq(2)").focus();
+            },
+            resizable: false,
+            height: 200,
+            width: 400,
+            modal: true,
+            closeOnEscape: false,
+            buttons: {
+                "<?php echo $langs->trans('Yes'); ?>": function() {
+                    location.href = href;
+                    $(this).dialog("close");
+                },
+                "<?php echo $langs->trans('No'); ?>": function() {
+                    $(this).dialog("close");
+                }
+            }
+        });
     });
 });
