85f1a4000ba813566835421e312fefbf8fc0d4f4
AXeL-dev <contact.axel.dev@gmail.com>
2019-07-29 11:49:13 +0100

    Release v1.0.1

--- core/modules/modDAMB.class.php
+++ core/modules/modDAMB.class.php
@@ -29,7 +29,7 @@ class modDAMB extends DolibarrModules
         $this->name            = 'AdvancedModuleBuilder';
         $this->description     = 'AdvancedModuleBuilderDesc';
         $this->picto           = 'module.png@damb';
-        $this->version         = '1.0.0';
+        $this->version         = '1.0.1';
         $this->const_name      = get_constant_name($this);
         $this->special         = 0;
 
